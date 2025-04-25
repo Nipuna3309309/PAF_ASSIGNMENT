b133fa0ec90a36d2c75f44c152b25cf994182c34
nipuna <IT22154576@my.sliit.lk>
2025-04-25 08:22:06 +0530

    updates to the put function

---
+++ crud/crud/demo/src/main/java/com/example/demo/service/PostService.java
@@ -0,0 +1,20 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Post;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+public interface PostService {
+    Post createPost(String description, MultipartFile[] mediaFiles, boolean isVideo) throws IOException;
+    Post updatePost(String id, String description, MultipartFile[] mediaFiles, boolean isVideo) throws IOException;
+
+    List<Post> getAllPosts();
+
+    Optional<Post> getPostById(String id);
+
+    void deletePost(String id);
+
+}
