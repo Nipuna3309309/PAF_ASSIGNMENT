d303447f91897997cad4a64a94c737897574e727
B.G.W.T.N.Bandaranayake <129357634+IT22247018@users.noreply.github.com>
2025-05-16 22:36:20 +0530

    update app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,47 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage.jsx";
-import RegisterPage from "./pages/RegisterPage.jsx";
-import LoginPage from "./pages/LoginPage.jsx";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import HomePage from "./pages/HomePage";
+import MyCourseDetail from "./pages/MyCourseDetail";
+import LearningProgressPage from "./pages/LearningProgressPage";
+import UnauthorizedPage from "./pages/UnauthorizedPage";
+import CertificatePage from "./pages/CertificatePage";
+import ProtectedRoute from "./routes/ProtectedRoute";
+import AdminDashboard from "./pages/AdminDashboard";
 import LearningPlanPage from './pages/LearningPlanPage';
 import LearningPlanDetailPage from './pages/LearningPlanDetailPage';
 import AIGenerateTasksPage from "./pages/AIGenerateTasksPage";
 import ShareLearningPlan from './pages/ShareLearningPlan';
+
 function App() {
   return (
     <>
-        <Routes>
-          <Route path="/certificate/:courseId" element={<CertificatePage />} />
-          <Route path="/learning-progress" element={<LearningProgressPage />} />
-          <Route path="/my-course/:courseId" element={<MyCourseDetail />} />
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/unauthorized" element={<UnauthorizedPage />} />
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/certificate/:courseId" element={<CertificatePage />} />
+        <Route path="/learning-progress" element={<LearningProgressPage />} />
+        <Route path="/my-course/:courseId" element={<MyCourseDetail />} />
 
-           <Route path="/learningplan" element={<LearningPlanPage />} />
+        <Route path="/unauthorized" element={<UnauthorizedPage />} />
+
+<Route path="/learningplan" element={<LearningPlanPage />} />
           <Route path="/learningplan/view/:planId" element={<LearningPlanDetailPage />} />
           <Route path="/aigenerate-tasks" element={<AIGenerateTasksPage />} />
           <Route path="/learningplan/share/:planId" element={<ShareLearningPlan />} />
 
-        </Routes>
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute allowedRoles={["admin"]}>
+              <AdminDashboard />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+
     </>
   );
 }
-
-export default App;
